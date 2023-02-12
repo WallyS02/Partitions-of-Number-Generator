ce25a508a7247d3c384f8f620e9c854512a10917
Sebastian Kutny <sebkutny@gmail.com>
2023-02-12 10:35:34 +0100

    Final Version

---
+++ README.md
@@ -0,0 +1,58 @@
+# AISD-Project-0
+partitions - recurrence
+
+Podział liczby n to reprezentacja tej liczby jako sumy dodatnich liczb całkowitych: n=n1+n2+...+nr, takich że n1≥n2≥...≥nr. Liczby n1,n2,...,nr nazywamy składnikami.
+
+Podział 19=5+3+3+2+2+2+2 możemy zapisać jako 53224, natomiast podział 19=5+2+2+2+2+2+2+2 możemy zapisać jako 527.
+Stąd indeksy górne w takim zapisie to odpowiednio 124 oraz 107, tak więc 5+3+3+2+2+2+2 jest większe od 5+2+2+2+2+2+2+2, 
+ponieważ 124 jest większe od 107 (patrząc z lewej pierwsza nieidentyczna pozycja zawiera 2 i 0, a 2>0, stąd 124>107).
+Jeśli nie zrozumiałeś, zerknij na zapis w zasadniczym twierdzeniu arytmetyki.
+
+Zadanie
+
+Rozpoczynając od najmniejszego podziału, wypisz wszystkie podziały liczby naturalnej n, składające się z liczb pierwszych, z których największa to k. 
+
+Uwaga:
+Ze względu na złożoność obliczeniową problemu liczba n zmieści się w bajcie.
+
+
+
+English version
+Introduction
+
+A partition of n is a representation of this number as a sum of positive integers: n=n1+n2+...+nr such that n1≥n2≥...≥nr. The numbers n1,n2,...,nr are called parts.
+
+The partition 19=5+3+3+2+2+2+2 can be written as 53224, while 19=5+2+2+2+2+2+2+2 can be written as 527.
+Hence the upper indices are 124 and 107, thus 5+3+3+2+2+2+2 is larger than 5+2+2+2+2+2+2+2, 
+since 124 is larger than 107.
+
+
+Problem
+
+Starting from the smallest partition, write all partitions of the integer n, which parts are prime numbers and the largest part is equal to k. n can be stored in byte.
+
+Input
+
+In the first line: a number of the rest lines
+Then, separated by spaces: n and k
+
+Output
+
+Partitions separated by +. If there is no partition, then write nothing.
+
+Example
+
+Input
+3
+12 5
+5 2
+27 17
+
+Output
+5+3+2+2
+5+5+2
+17+2+2+2+2+2
+17+3+3+2+2
+17+5+3+2
+17+5+5
+17+7+3
---
+++ main.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define MAX_AMOUNT_PRIME 54
+#define MAX_NUMBER 256
+
+int findMinIndex(int* result, int size) {
+    int minIndex = 0;
+    for (int i = 0; i <= size; i++)
+        if (result[i] != 0) {
+            minIndex = i;
+            break;
+        }
+    return minIndex;
+}
+
+void print(int* result, int size, int tab[]) {
+    int minIndex = findMinIndex(result, size);
+    for (int i = size; i > minIndex; i--) {
+        for (int j = 0; j < result[i]; j++) {
+            cout << tab[i] << "+";
+        }
+    }
+    for (int i = 0; i < result[minIndex] - 1; i++)
+        cout << tab[minIndex] << "+";
+    cout << tab[minIndex];
+    cout << "\n";
+}
+
+void searchForPartition(int n, int l, int tab[], int* result, int size) {
+    if (n == 0) {
+        print(result, size, tab);
+        return;
+    }
+    for (int i = 0; i <= l; i++) {
+        if (n >= tab[i]) {
+            result[i]++;
+            searchForPartition(n - tab[i], i, tab, result, size);
+            result[i]--;
+        }
+    }
+}
+
+void erastotenes(int n, int tab[]) {
+    bool pom[MAX_NUMBER+1];
+    for (int i = 2; i <= n; i++) {
+        pom[i] = 0;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (!pom[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                pom[j] = 1;
+            }
+        }
+    }
+    int p = 0;
+    for (int i = 2; i <= n; i++)
+        if (!pom[i]) {
+            tab[p] = i;
+            p++;
+        }
+} /*procedure from - https://pl.wikipedia.org/wiki/Sito_Eratostenesa */
+
+int binarySearch(int left, int right, int searched, int tab[]) {
+    while (left <= right) {
+        int middle = (left + right) / 2;
+        if (tab[middle] == searched)
+            return middle;
+        if (tab[middle] < searched)
+            left = middle + 1;
+        else
+            right = middle - 1;
+    }
+    return -1;
+} /*procedure from - https://pl.wikipedia.org/wiki/Wyszukiwanie_binarne */
+
+int main() {
+    int tab[MAX_AMOUNT_PRIME];
+    erastotenes(MAX_NUMBER, tab);
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int n, k, l;
+        cin >> n;
+        cin >> k;
+        l = binarySearch(0, MAX_AMOUNT_PRIME, k, tab);
+        n -= k;
+        int* result = new int[l+1];
+        result[l] = 1;
+        for (int j = 0; j < l; j++) {
+            result[j] = 0;
+        }
+        if(l != -1)
+            searchForPartition(n, l, tab, result, l);
+        delete[] result;
+    }
+    return 0;
+}
